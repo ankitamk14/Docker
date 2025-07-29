841d077d34080ead07b8c21c15eecfe61683197b
ankitamk14 <ankitamk@gmail.com>
2025-07-29 18:20:56 +0530

    models

--- dockerdemo/demo/views.py
+++ dockerdemo/demo/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from .models import Book
 
 def index(request):
-    return HttpResponse("Hello from demo app!")
+    books_count = Book.objects.count()
+    return HttpResponse(f"Hello from demo app! : {books_count}")
